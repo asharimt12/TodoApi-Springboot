6d34a195ef9f18ee73feb8ab83b17984e2cf479f
Ashar <asharimt12@gmail.com>
2026-04-30 05:33:54 +0530

    Added delete and patch method

--- src/main/java/com/example/TodoApiSpring/TodoController.java
+++ src/main/java/com/example/TodoApiSpring/TodoController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/todos/")
 public class TodoController {
     private static List<Todo> todoList;
+
     public TodoController(){
         todoList = new ArrayList<>();
         todoList.add(new Todo(1, false, "Todo 1", 1));
@@ -30,11 +32,66 @@ public class TodoController {
     }
 
     @GetMapping("/{todoId}")
-    public ResponseEntity<?> getTodoById(@PathVariable Long todoId){
+    public ResponseEntity<?> getTodoById(@PathVariable long todoId){
         for(Todo todo: todoList){
            if (todo.getId() == todoId){
                return ResponseEntity.ok(todo);
            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new NotFoundResponse(todoId));
+    }
+
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<?> deleteTodo(@PathVariable long todoId){
+        for(Todo todo: todoList){
+            if (todo.getId() == todoId){
+                todoList.remove(todo);
+                return ResponseEntity.ok(todoList);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new NotFoundResponse(todoId));
+    }
+/*
+    @PatchMapping("/{todoId}")
+    public ResponseEntity<?> patchTodo(@PathVariable long todoId,
+                                       @RequestBody Map<String, Object> updates){
+        for (Todo todo: todoList){
+            if (todo.getId() == todoId){
+                if (updates.containsKey("id")){
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Illegal to update id...");
+                }
+                updates.forEach((key, value) -> {
+                    switch (key){
+                        case "title" -> todo.setTitle((String) value);
+                        case "completed" -> todo.setCompleted((boolean) value);
+                        case "userId" -> todo.setUserId((int) value);
+                    }
+                });
+                return ResponseEntity.ok(todo);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new NotFoundResponse(todoId));
+    }
+*/
+    //Another way to do above is by using query params
+    @PatchMapping("/{todoId}")
+    public ResponseEntity<?> patchTodos(@PathVariable long todoId,
+                                        @RequestParam(required = false) Boolean isCompleted,
+                                        @RequestParam(required = false) Integer userId,
+                                        @RequestParam(required = false) String title){
+        for(Todo todo: todoList){
+            if(todo.getId() == todoId){
+                if(isCompleted != null){
+                    todo.setCompleted(isCompleted);
+                }
+                if(title != null){
+                    todo.setTitle(title);
+                }
+                if(userId != null){
+                    todo.setUserId(userId);
+                }
+                return ResponseEntity.ok(todo);
+            }
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new NotFoundResponse(todoId));
     }
